6a6870d1651504fc146f01d3f532a41506d66f8f
Sheikh-Foysal <openoway@gmail.com>
2020-01-13 23:43:03 +0600

    component organized

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,23 @@
 
 require('./bootstrap');
-
 window.Vue = require('vue');
 
+// Vue Router
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
+import {routes} from './routes';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('admin-main', require('./components/admin/AdminMaster.vue').default);
 
-
-const routes = [
-    // { path: '/home', component:  }
-  ]
 
 const router = new VueRouter({
-    routes 
+    routes,
+    mode: 'history' 
   })
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
 });
