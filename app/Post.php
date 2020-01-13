094b959cda6a5d32c77a16219dd544d33763eb2c
foysal <openoway@gmail.com>
2020-01-13 16:47:22 +0600

    Post controller and relaionship made

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Category;
 class Post extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class,'id');
+    }
+    
+    public function category(){
+        return $this->hasMany(Category::class,'id','cat_id');
+    }
 }
