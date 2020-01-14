d116deec953df77186cfcd5127fa164e1b9a3057
foysal <openoway@gmail.com>
2020-01-14 12:33:37 +0600

    laravel mix route part done

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,7 +1,12 @@
 import AdminHome from './components/admin/AdminHome.vue'
+import Ex from './components/ExampleComponent.vue'
 export const routes = [
     { 
         path: '/home',
         component:AdminHome
+    },
+    {
+        path: '/posts',
+        component: Ex
     }
   ]
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/posts','PostController@all_posts')->name('posts');
+
+// Route::get('/{anypath}','HomeController@index')->where('path','.*');
+Route::get('/{any}','HomeController@index')->where('any', '.*');
